30e0add970a95d488e7efa537e1a4dff5ac5d142
rabotnik1 <randall@posteo.de>
2024-12-23 20:45:31 +1000

    gradients and svg background added

---
+++ src/app/january/page.tsx
@@ -0,0 +1,9 @@
+import "./page.css";
+
+export default function January() {
+  return (
+    <div className="page-container">
+      <div className="main-background"></div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,22 @@ type Artist = {
 }
 
 export default async function Home() {
-  const url =  "https://sternstunde.fly.dev" // "http://localhost:8080" //
+  // const url =  "https://sternstunde.fly.dev" 
+  const url =   "http://localhost:8080" //
   const headers = { Accept: "application/json" };
   const response = await fetch(`${url}/get-artists`, { headers });
   const artists: Artist[] = await response.json();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div>
       <h1>Sternstunden deploy test site!</h1>
+      <div className="size-full flex flex-wrap items-center gap-8">
       {artists.map(a => (
-        <div key={a.name}>
+        <div key={a.name} className="size-64 overflow-hidden">
           <img src={a.imageUrl} alt={a.name} />
-          {a.name}</div>
+          {a.name}
+        </div>
       ))}
+    </div>
     </div>
   );
 }
